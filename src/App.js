b9c263d34374f534bb8cd20e71db869c0e64a31b
Scott Rees <6165315+reesscot@users.noreply.github.com>
2023-10-10 15:59:43 -0700

    chore: show how interval indeed refreshes the user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Amplify, Hub } from 'aws-amplify';
+import { Amplify, Hub, Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
@@ -9,12 +9,22 @@ Amplify.configure(awsExports);
 function App({ signOut, user }) {
 
   useEffect(()=> {
+    Auth.currentAuthenticatedUser().then((user) => {
+      console.log('first user', user);
+    });
+    console.log('setup hub event listener in useeffect')
 
-    console.log('setup hub event listener')
+    setInterval(() => {
+      Auth.currentAuthenticatedUser().then((user) => {
+        console.log('refreshed user', user);
+      });
+      console.log('refresh token');
+    }, 5000);
+
     Hub.listen('auth', (data) => {
       console.log('auth event listener', data);
       if(data.payload.event === 'tokenRefresh') {
-        console.log('token refreshed');
+        console.log('token refreshed', data);
       }
     });
   }, [])
